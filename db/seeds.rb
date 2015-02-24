cea9817d1ef1ceb682c9c804a9eb91046f9bdc24
Justin Herrick <justin@justinherrick.com>
2015-02-24 15:27:45 -0600

    Adding models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+5.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
+  5.times do
+    user.posts.create(title: Faker::Lorem.sentence(1))
+  end
+end
+
+15.times do
+  Comment.create(user_id: rand(1..5), post_id: rand(1..25), title: Faker::Lorem.sentence(1))
+end
